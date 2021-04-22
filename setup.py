29f66be69ed2c20c501f7ed9cdbcf7345deca452
gistrec <gistrec@mail.ru>
2021-04-22 17:36:13 +0700

    Updated PyPi project description

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@ from setuptools import find_packages, setup
 
 LONGDESC = """
 Git-Hash can be used to create commit hash by mask.
+
 Website: https://github.com/gistrec/git-hash
+
 Author:  Aleksandr Kovalko <gistrec@mail.ru>
 """
 
 setup(
     name="git-hash",
-    version="1.0.0",
+    version="1.0.2",
     description='Create commit hash by mask',
     license='Apache License 2.0',
     author="Aleksandr Kovalko",
     author_email="gistrec@mail.ru",
-    url="",
+    url="https://github.com/gistrec/git-hash",
     platforms='Any',
     install_requires=[
         'optparse-pretty'
@@ -29,8 +31,10 @@ setup(
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Programming Language :: Python",
-        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Version Control",
         "Topic :: Utilities"],
     keywords='git hash commit'
 )
